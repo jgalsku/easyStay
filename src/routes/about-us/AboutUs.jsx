23fdfa00a2dbdba7e08ae5d836f1f2ba97db615d
karimoraga <moka.25mk@gmail.com>
2024-09-28 21:13:09 -0300

    about1
    texto

--- src/routes/about-us/AboutUs.jsx
+++ src/routes/about-us/AboutUs.jsx
@@ -7,39 +7,30 @@ import React from 'react';
 const AboutUs = () => {
   return (
     <div className="container mx-auto px-4 py-8">
-      <h1 className="text-4xl font-extrabold text-brand mb-2">About Us</h1>
+      <h1 className="text-4xl font-extrabold text-brand mb-2">Sobre nosotros</h1>
       <p className="text-lg mb-8">
         Bienvenido a <span className="text-brand">PACIFIC REEF</span>, donde esperamos 
-        proveerte la mejor experiencia para reservar habitaciones en hoteeles a lo largo del mundo
+        proveerte la mejor experiencia para reservar habitaciones en hoteles a lo largo del mundo
       </p>
 
       <h2 className="text-3xl font-extrabold text-brand mb-2">Our Vision</h2>
       <p className="text-lg mb-8">
-        At <span className="text-brand">STAY BOOKER</span>, we envision a world
-        where every traveler finds the perfect accommodation that suits their
-        needs and preferences. We aim to simplify the hotel booking process,
-        offering a wide range of options for every budget.
+        En <span className="text-brand">EASY STAY</span>, donde nuestro objetivo es un mundo en el que
+        cada viajero encuentre las herramientas perfectas que satisfagan sus necesidades y preferencias.
+        Apuntamos a que simplificar el proceso de reserva, ofreciendo un amplio rango de opciones para todos los bolsillos
       </p>
 
       <h2 className="text-3xl font-extrabold text-brand mb-2">
-        Why Choose Us?
+        Porqué nosotros?
       </h2>
       <ul className="list-disc ml-6 mb-8">
         <li className="text-lg mb-3">
-          We offer a diverse range of hotels, from luxury resorts to cozy
-          boutique stays, ensuring that you find the perfect match for your
-          travel style.
+          Ofrecemos un diverso rango de hoteles, desde lujosos a comodos, asegurando satisfacer tus necesidades personales
         </li>
         <li className="text-lg mb-3">
-          Our user-friendly interface makes it simple and quick to book your
-          ideal stay. With just a few clicks, you can secure your reservation
-          hassle-free.
+          Nuestra interfaz amigable hace simple y rapido agendar tu habitación ideal, con apenas unos clicks puedes asegurar tu reserva sin molestias
         </li>
-        <li className="text-lg mb-3">
-          Our dedicated customer support team is available 24/7 to assist you
-          with any inquiries or issues you may encounter during your booking
-          process or stay.
-        </li>
+       
         <li className="text-lg mb-3">
           We prioritize the security of your personal information and
           transactions. Book with confidence, knowing that your data is safe
@@ -47,18 +38,8 @@ const AboutUs = () => {
         </li>
       </ul>
 
-      <h2 className="text-3xl font-extrabold text-brand mb-2">Contact Us</h2>
-      <p className="text-lg mb-4">
-        Have questions or need assistance? Feel free to reach out to our
-        customer support team at{' '}
-        <a
-          className="text-brand hover:underline"
-          href="mailto:info@staybooker.com"
-        >
-          info@staybooker.com
-        </a>
-        . We're here to help!
-      </p>
+  
+     
       <p className="text-lg">
         Thank you for choosing <span className="text-brand">STAY BOOKER</span>.
         We look forward to being your go-to platform for all your hotel booking
